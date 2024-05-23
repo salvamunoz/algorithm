7c977990e66db2b33d82d05159bbae5bc2ef0928
Salva <salva17y2@gmail.com>
2024-05-23 14:07:22 +0200

    adding time decorator

--- easy/factorial.py
+++ easy/factorial.py
@@ -1,12 +1,15 @@
 # program that will calculate the factorial of a given number
 
+from get_time import timing
+
+@timing
 def iterative_factorial(n: int) -> int:
     fact = 1
     for i in range(2, n + 1):
         fact *= i
     return fact
 
-
+@timing
 def recursive_factorial(n: int) -> int:
     if n == 1:
         return n
